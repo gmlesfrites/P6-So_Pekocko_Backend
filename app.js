d71e17edf6bb0289d94537420160db5019323974
Emilie <egcommunreflex@gmail.com>
2020-07-01 13:03:50 +0200

    ajout middleware pour cors

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+//Creation du serveur avec framework Express
 const express = require('express');
-
 const app = express();
 
-app.use((req, res) => {
+//Middleware pour autoriser certains headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+//Middleware classique -> ôter next si dernier middleware
+app.use((req, res, next) => {
     res.json({ message: 'votre requête a bien été reçue' });
 })
+
+//Export de l'app Express pour utilisation server.js
 module.exports = app;
