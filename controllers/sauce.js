1978dfc4630f28aefd465642868ef17a7068b712
Emilie <egcommunreflex@gmail.com>
2020-07-07 16:38:39 +0200

    précisions sur les commentaires

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,7 +1,7 @@
 //Importation du modèle sauce
 const Sauce = require("../models/Sauce");
 
-//création d'une sauce - route L12
+//Middleware de création d'une sauce - route L12
 exports.createSauce = (req, res, next) => {
     delete req.body._id;
     const sauce = new Sauce({
@@ -12,28 +12,28 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 };
 
-//récupération des sauces par id - route L15
+//Middleware de récupération des sauces par id - route L15
 exports.createOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }));
 };
 
-//modification d'une sauce (par id) - route L18
+//Middleware de modification d'une sauce (par id) - route L18
 exports.modifySauce = (req, res, next) => {
     Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
         .catch(error => res.status(400).json({ error }));
 };
 
-//suppression d'une sauce (par id) - route L21
+//Middleware de suppression d'une sauce (par id) - route L21
 exports.deleteSauce = (req, res, next) => {
     Sauce.deleteOne({ _id: req.params.id })
         .then(() => { res.status(200).json({ message: 'Deleted!' }); })
         .catch((error) => { res.status(400).json({ error: error }); });
 };
 
-//récupération des sauces - route L9
+//Middleware de récupération des sauces - route L9
 exports.createSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
